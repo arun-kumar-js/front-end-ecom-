77e8b86784afccdf8deccf83cb668d60acf4c2c2
arun-kumar-js <srivar63@outlook.com>
2025-02-05 21:41:54 +0530

    6

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
- 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'react-toastify/dist/ReactToastify.css'
-import { RouterProvider } from 'react-router'
-import router from './Router.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { RouterProvider } from "react-router-dom"; // ✅ Correct import
+import router from "./Router.jsx";
 import { ToastContainer } from "react-toastify";
-import { Provider } from 'react-redux'
-import store from './Redux/app/store.js'
+import { Provider } from "react-redux";
+import store from "./Redux/app/store.js";
 
 createRoot(document.getElementById("root")).render(
-  < Provider store={store}>
+  <Provider store={store}>
+    {" "}
+    {/* Removed extra space after "<" */}
     <RouterProvider router={router} />
-    < ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl=
-      {false} pauseOnFocusLoss draggable pauseOnHover />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </Provider>
 );
