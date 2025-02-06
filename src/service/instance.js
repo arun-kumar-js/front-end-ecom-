28ff7da2406714bf9149ec7ca65169a5f07cab13
arun-kumar-js <srivar63@outlook.com>
2025-02-06 14:52:52 +0530

    deploy

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,13 +1,6 @@
 import axios from "axios";
-
-// Assuming JWT token is stored in localStorage (you can modify this as per your setup)
-const token = localStorage.getItem("jwtToken");  // Modify this based on where you store your JWT token
-
-const baseURL = [
-  "http://localhost:3000/auth",
-  "https://project-be-lkrg.onrender.com/",
-];
-
+import e from "cors";
+const baseURL = "https://project-be-qvrk.onrender.com/auth";
 const instance = axios.create({
   baseURL: baseURL,
   timeout: 30000,
@@ -16,19 +9,5 @@ const instance = axios.create({
   },
   withCredentials: true,
 });
-
-// Interceptor to add JWT token to each request if available
-instance.interceptors.request.use(
-  (config) => {
-    if (token) {
-      // Add the token to the Authorization header if token exists
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default instance;
