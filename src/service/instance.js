ffddc2a6d42162f327ba69a260f01e34084683af
arun-kumar-js <srivar63@outlook.com>
2025-02-06 13:37:35 +0530

    finalll

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,14 +1,27 @@
 import axios from "axios";
 import e from "cors";
-const baseURL = "https://project-be-lkrg.onrender.com/auth";
+const baseURL = "http://localhost:3000/auth";
 const instance = axios.create({
   baseURL: baseURL,
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
-  
   },
   withCredentials: true,
 });
+
+// Interceptor to add JWT token to each request if available
+instance.interceptors.request.use(
+  (config) => {
+    if (token) {
+      // Add the token to the Authorization header if token exists
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
